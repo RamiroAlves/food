328e5f6d68409610bd0fb146908d3d0c74d3e432
RamiroAlves <ramiro.junior25@gmail.com>
2024-10-07 22:02:04 -0300

    Iniciado projeto, configurado tailwind, adicionado componente de header, componente de banner e componente de search

---
+++ src/app/index.tsx
@@ -0,0 +1,24 @@
+import { ScrollView, Text, View } from "react-native";
+import { Header } from "../components/header";
+
+import Constants from "expo-constants";
+import { Banner } from "../components/banner";
+import { Search } from "../components/search";
+
+const statusBarHeight = Constants.statusBarHeight;
+
+export default function Index() {
+  return (
+    <ScrollView
+      style={{ flex: 1 }}
+      className="bg-slate-200"
+      showsVerticalScrollIndicator={false}
+    >
+      <View className="w-full px-4" style={{ marginTop: statusBarHeight + 12 }}>
+        <Header />
+        <Banner />
+        <Search />
+      </View>
+    </ScrollView>
+  );
+}
